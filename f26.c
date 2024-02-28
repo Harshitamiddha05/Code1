13992f5f3d27e464da9bb283caaaae295072314f
Harshitamiddha05 <145043751+Harshitamiddha05@users.noreply.github.com>
2024-02-28 10:42:51 +0530

    Update f26.c

--- f26.c
+++ f26.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    char s[100];
-    int length = 0, i = 0;
-
-    printf("Enter a string: ");
-    fgets(s, sizeof(s), stdin);
-
-    for (i = 0; s[i] != '\0'; i++) {
-        if (s[i] == ' ') {
-            length++;
-        }
-    }
-
-    // Increment count for the last word
-    length++;
-
-    printf("Number of words present in array is %d\n", length);
-
-    printf("Content of the string: %s\n", s);
-
-    return 0;
+int main()
+{
+char s[100];
+int length = 0, i = 0;
+printf("Enter a string: ");
+fgets(s, sizeof(s), stdin);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == ' ')
+{
+length++;
+}
+}
+length++;
+printf("Number of words present in array is %d\n", length);
+printf("Content of the string: %s\n", s);
+return 0;
 }
