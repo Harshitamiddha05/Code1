070862393e463478504487206a17fdfab897bb38
Harshitamiddha05 <145043751+Harshitamiddha05@users.noreply.github.com>
2024-03-04 18:59:27 +0530

    Add files via upload

---
+++ f34.c
@@ -0,0 +1,27 @@
+/*write a c program to insert elements in two different strings and concatenate or merge these two strings and display the content*/
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+char str[100], str1[100], concatstr[300];
+int i, j, l, l1;
+printf("Input a string: ");
+fgets(str, 100, stdin);
+printf("Input the second string: ");
+fgets(str1, 100, stdin);
+l = strlen(str);
+if (str[l - 1] == '\n')
+str[l - 1] = '\0';
+l1 = strlen(str1);
+if (str1[l1 - 1] == '\n')
+str1[l1 - 1] = '\0';
+for (i = 0; str[i] != '\0'; i++)
+concatstr[i] = str[i];
+for (j = 0; str1[j] != '\0'; j++)
+concatstr[i + j] = str1[j];
+concatstr[i + j] = '\0';
+printf("String 1: %s\n", str);
+printf("String 2: %s\n", str1);
+printf("Concatenated string: %s\n", concatstr);
+return 0;
+}
